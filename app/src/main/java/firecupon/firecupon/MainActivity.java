954ed46001bc30358e0634056150dac0b736bb43
bulineta <needagadget@gmail.com>
2017-06-03 23:42:51 +0300

    Initial commit

---
+++ app/src/main/java/firecupon/firecupon/MainActivity.java
@@ -0,0 +1,21 @@
+package firecupon.firecupon;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonOnClick(View v){
+        Button Button=(Button) v;
+        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+    }
+}
