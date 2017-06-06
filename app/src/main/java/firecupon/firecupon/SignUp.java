0f518d765b34c71e44c9bb06601a7aa76964e300
bulineta <needagadget@gmail.com>
2017-06-06 23:11:13 +0300

    modules1

--- app/src/main/java/firecupon/firecupon/SignUp.java
+++ app/src/main/java/firecupon/firecupon/SignUp.java
@@ -6,26 +6,29 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUp extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "EmailPassword";
 
     Button btnSignup;
     TextView btnLogin,btnForgotPass;
     EditText input_email,input_pass;
     RelativeLayout activity_sign_up;
 
-    private FirebaseAuth auth;
+    private FirebaseAuth mAuth;
     Snackbar snackbar;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,13 +47,13 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
         btnForgotPass.setOnClickListener(this);
 
         //init firebase
-        auth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
     }
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.btn_login) {
+        if(v.getId() == R.id.signup_btn_login) {
             startActivity(new Intent(SignUp.this, LoginActivity.class));
             finish();
         }
@@ -64,20 +67,30 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
     }
 
     private void signUpUser(String email, String password) {
-        auth.createUserWithEmailAndPassword(email, password)
+
+        mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(!task.isSuccessful())
-                        {
-                            snackbar = Snackbar.make(activity_sign_up, "Error: "+task.getException(),Snackbar.LENGTH_SHORT);
-                            snackbar.show();
-                        }
-                        else{
-                            snackbar = Snackbar.make(activity_sign_up,"Register success : ",Snackbar.LENGTH_SHORT);
-                            snackbar.show();
+                        if(!task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "Registration Failed: ");
+                            Toast.makeText(SignUp.this, "Register sadas",
+                                    Toast.LENGTH_SHORT).show();
+                            FirebaseUser currentUser = mAuth.getCurrentUser();
+                            updateUI(currentUser);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignUp.this, "Register Success",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
 
                         }
+                    }
+
+                    private void updateUI(FirebaseUser currentUser) {
+
                     }
                 });
     }
